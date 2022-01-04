9b8bf6a831eb3a1b7dae1b0b2dfddc899d2da74e
Adarsh Prajapati <imadarsh.ap@gmail.com>
2022-01-04 20:24:08 +0530

    Product List Dynamic DataTable using Ajax

--- source/resources/views/admin/product/list.blade.php
+++ source/resources/views/admin/product/list.blade.php
@@ -6,128 +6,185 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/scroller/2.0.3/js/dataTables.scroller.min.js">
 
 <style>
-    .material-icons{
-        margin-top:0px !important;
-        margin-bottom:0px !important;
+    .material-icons {
+        margin-top: 0px !important;
+        margin-bottom: 0px !important;
     }
 </style>
 @section ('content')
 <div class="container-fluid">
-    
- <div class="row">
-<div class="col-lg-12">
-@if (session()->has('success'))
-<div class="alert alert-success">
-@if(is_array(session()->get('success')))
-        <ul>
-            @foreach (session()->get('success') as $message)
-                <li>{{ $message }}</li>
-            @endforeach
-        </ul>
-        @else
-            {{ session()->get('success') }}
-        @endif
+
+    <div class="row">
+        <div class="col-lg-12">
+            @if (session()->has('success'))
+            <div class="alert alert-success">
+                @if(is_array(session()->get('success')))
+                <ul>
+                    @foreach (session()->get('success') as $message)
+                    <li>{{ $message }}</li>
+                    @endforeach
+                </ul>
+                @else
+                {{ session()->get('success') }}
+                @endif
+            </div>
+            @endif
+            @if (count($errors) > 0)
+            @if($errors->any())
+            <div class="alert alert-danger" role="alert">
+                {{$errors->first()}}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            @endif
+            @endif
+        </div>
+        <div class="col-lg-12">
+
+            <a href="{{route('AddProduct')}}" class="btn btn-primary ml-auto" style="width:15%;float:right;padding: 3px 0px 3px 0px;"><i class="material-icons">add</i>Add Product</a>
+        </div>
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header card-header-primary">
+                    <h4 class="card-title ">Products List</h4>
+                </div>
+                <div class="container"><br>
+                    <table class="display" id="myTable">
+                        <thead>
+                            <tr>
+                                <th>Product id</th>
+                                <th>Product Image</th>
+                                <th>Product Name</th>
+                                <th>Category</th>
+                                <th>Hide</th>
+                                <th class="text-right">Actions</th>
+                            </tr>
+                        </thead>
+                        <!-- <tbody>
+                            @if(count($product)>0)
+                            @php $i=1; @endphp
+                            @foreach($product as $products)
+                            <tr>
+                                <td class="text-center">{{$i}}</td>
+                                <td>{{$products->product_name}}</td>
+                                <td>{{$products->product_id}}</td>
+                                <td> {{$products->title}}</td>
+                                <td><img src="{{url($products->product_image)}}" alt="image" style="width:50px;height:50px; border-radius:50%" /></td>
+                                <td><input type="checkbox" data-id="{{ $products->product_id }}" name="status" class="js-switch" {{ $products->hide == 1 ? 'checked' : '' }}></td>
+                                <td class="td-actions text-right">
+                                    <a href="{{route('EditProduct',$products->product_id)}}" rel="tooltip" class="btn btn-success">
+                                        <i class="material-icons">edit</i>
+                                    </a>
+                                    <a href="{{route('varient',$products->product_id)}}" rel="tooltip" class="btn btn-primary">
+                                        <i class="material-icons">layers</i>
+                                    </a>
+                                    <a href="{{route('DeleteProduct',$products->product_id)}}" onclick="if(!confirm('Are you sure, You want delete this Product?')){return false;}" rel="tooltip" class="btn btn-danger">
+                                        <i class="material-icons">close</i>
+                                    </a>
+                                </td>
+                            </tr>
+                            @php $i++; @endphp
+                            @endforeach
+                            @else
+                            <tr>
+                                <td>No data found</td>
+                            </tr>
+                            @endif
+                        </tbody> -->
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
-@endif
- @if (count($errors) > 0)
-  @if($errors->any())
-    <div class="alert alert-danger" role="alert">
-      {{$errors->first()}}
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">×</span>
-      </button>
-    </div>
-  @endif
-@endif
 </div>
-<div class="col-lg-12"> 
+<div>
+</div>
+<script>
+    function setJsSwicth() {
+        let elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
 
-     <a href="{{route('AddProduct')}}" class="btn btn-primary ml-auto" style="width:15%;float:right;padding: 3px 0px 3px 0px;"><i class="material-icons">add</i>Add Product</a>
-</div> 
-<div class="col-lg-12">
-<div class="card">    
-<div class="card-header card-header-primary">
-      <h4 class="card-title ">Products List ({{count($product)}} Products)</h4>
-    </div>
-<div class="container"><br>    
-<table class="display" id="myTable">
-    <thead>
-        <tr>
-            <th class="text-center">#</th>
-            <th>Product Name</th>
-            <th>Product id</th>
-            <th>Category</th>
-            <th>Product_image</th>
-            <th>Hide</th>
-            <th class="text-right">Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-           @if(count($product)>0)
-          @php $i=1; @endphp
-          @foreach($product as $products)
-        <tr>
-            <td class="text-center">{{$i}}</td>
-            <td>{{$products->product_name}}</td>
-            <td>{{$products->product_id}}</td>
-            <td> {{$products->title}}</td>
-            <td><img src="{{url($products->product_image)}}" alt="image"  style="width:50px;height:50px; border-radius:50%"/></td>
-            <td><input type="checkbox" data-id="{{ $products->product_id }}" name="status" class="js-switch" {{ $products->hide == 1 ? 'checked' : '' }}></td>
-            <td class="td-actions text-right">
-                <a href="{{route('EditProduct',$products->product_id)}}" rel="tooltip" class="btn btn-success">
-                    <i class="material-icons">edit</i>
-                </a>
-                <a href="{{route('varient',$products->product_id)}}" rel="tooltip" class="btn btn-primary">
-                    <i class="material-icons">layers</i>
-                </a>
-                <a href="{{route('DeleteProduct',$products->product_id)}}" onclick="if(!confirm('Are you sure, You want delete this Product?')){return false;}" rel="tooltip" class="btn btn-danger">
-                    <i class="material-icons">close</i>
-                </a>
-            </td>
-        </tr>
-          @php $i++; @endphp
-                 @endforeach
-                  @else
-                    <tr>
-                      <td>No data found</td>
-                    </tr>
-                  @endif
-    </tbody>
-</table>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    <div>
-    </div>
-     <script>
-     
-     
-//      $(document).ready(function() {
-//     $('#myTable').DataTable({
-//         dom: 'Plfrtip',
-//         ajax: '../../../../examples/ajax/data/data_50k.txt',
-//         columns: [
-//             { data: "name" },
-//             { data: "position" },
-//             { data: "office" },
-//             { data: "extn" },
-//             { data: "start_date" },
-//             { data: "salary" }
-//         ],
-//         deferRender: true
-//     });
-// });
+        elems.forEach(function(html) {
+            let switchery = new Switchery(html, {
+                size: 'small'
+            });
+        });
 
-        $(document).ready( function () {
-            // $('#myTable').DataTable();
-            $('#myTable').DataTable( {
-                "processing": true,
-                "serverSide": true,
-                "ajax": "{{route('getProductList)}}"
-            } );
-            
+        $('.js-switch').change(function() {
+            let status = $(this).prop('checked') === true ? 1 : 0;
+            let product_id = $(this).data('id');
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: '{{ route("hideprod") }}',
+                data: {
+                    'status': status,
+                    'product_id': product_id
+                },
+                success: function(data) {
+                    console.log(data.message);
+                }
+            });
+        });
+    }
+
+    $(document).ready(function() {
+        // $('#myTable').DataTable();
+        $('#myTable').DataTable({
+            "processing": true,
+            "serverSide": true,
+            "ajax": "{{route('getProductList')}}",
+            "drawCallback": function(settings) {
+               setJsSwicth();
+            },
+            columns: [
+                {
+                    data: 'product_id'
+                },
+                {
+                    data: 'product_image',
+                    sortable: false,
+                    searchable: false,
+                    render: function(product_image) {
+                        return '<img src="{{url("/")}}/' + product_image + '" alt="image" style="width:50px;height:50px; border-radius:50%" />';
+                    }
+                },
+                {
+                    data: 'product_name'
+                },
+                {
+                    data: 'cat_name'
+                },
+                {
+                    data: {
+                        hide: 'hide',
+                        product_id: 'product_id'
+                    },
+                    sortable: false,
+                    searchable: false,
+                    render: function(data) {
+                        return '<input type="checkbox" data-id="' + data.product_id + '" name="status" class="js-switch" ' + ((data.hide == 1) ? "checked" : "") + '>';
+                    }
+                },
+                {
+                    data: 'product_id',
+                    sortable: false,
+                    searchable: false,
+                    render: function(product_id) {
+                        return '<div class="text-right"><a href="' + "{{url('product/edit')}}/" + product_id + '" rel="tooltip" class="btn btn-success">' +
+                            '<i class="material-icons">edit</i>' +
+                            '</a>' +
+                            '<a href="' + "{{url('varient')}}/" + product_id + '" rel="tooltip" class="btn btn-primary">' +
+                            '<i class="material-icons">layers</i>' +
+                            '</a>' +
+                            '<a href="' + "{{url('product/delete')}}/" + product_id + '" onclick="if(!confirm(' + "'" + 'Are you sure, You want delete this Product?' + "'" + ')){return false;}" rel="tooltip" class="btn btn-danger">' +
+                            '<i class="material-icons">close</i>' +
+                            '</a></div>';
+                    }
+                }
+            ]
+        });
+
         //      $('#myTable').DataTable( {
         //     data:           data,
         //     deferRender:    true,
@@ -135,42 +192,7 @@
         //     scrollCollapse: true,
         //     scroller:       true
         // } );
-        
-            
-            
-            
-        } );
-        
-        
-        
-       
-        
-        
-    </script>
-    <script>
-    let elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
-
-    elems.forEach(function(html) {
-    let switchery = new Switchery(html,  { size: 'small' });
-    });
-    </script>
-<script>
-    $(document).ready(function(){
-        $('.js-switch').change(function () {
-            let status = $(this).prop('checked') === true ? 1 : 0;
-            let product_id = $(this).data('id');
-            $.ajax({
-                type: "GET",
-                dataType: "json",
-                url: '{{ route("hideprod") }}',
-                data: {'status': status, 'product_id': product_id},
-                success: function (data) {
-                    console.log(data.message);
-                }
-            });
-        });
     });
 </script>
-     
-    @endsection
+@endsection
 </div>
